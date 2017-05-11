834f873fe643108973a9aac318961c785921ff69
jake waters <jerkey@g-cipher.net>
2017-05-11 01:18:51 -0700

    start taking notes on pinout

---
+++ tedwatch.ino
@@ -0,0 +1,12 @@
+TED MODEL: 1001 MTU
+100-130V~, 1W
+50/60Hz
+J4 is an 8-pin connector
+1 5.00v
+2 5.00v
+3 GND?  -side of C2 and C5
+4 4.99v
+5 4.99v
+6 4.99v
+7 weird pulses, saved a logic capture
+8 4.99v
