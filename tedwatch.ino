a5f24200fb8ac3004636cefcb2b80621a68bf162
jake waters <jerkey@g-cipher.net>
2017-05-16 19:34:12 -0700

    four digit numeric display worksgsgs!

--- tedwatch.ino
+++ tedwatch.ino
@@ -13,27 +13,34 @@ pin 22 (3rd from last) of U1 will also be low if direction is the other way
 #define VERSIONSTR "giant whatwatt display with YTD watt-hour numeric display"
 #define BAUDRATE 57600
 
-#include "ht1632c.h"
-#include "font1.h"
-#define HT1632_CS       6   // Chip Select (pin 1)
-#define HT1632_CLK      4   // Clk pin (pin 2)
-#define HT1632_WRCLK    12  // Write clock pin (pin 5)
-// Sun May 14 13:00 --
-#define HT1632_DATA     13  // Data pin (pin 7)
-#define DISPLAYRATE 2000 // how many milliseconds to show each text display before switching
+#define DISPLAYRATE 1000 // how many milliseconds to show each text display before switching
 
 #define ENERGYPULSE     0.0012 // this many watt-hours have been used
-float wattHours; // stores total counted energy
+volatile float wattHours; // stores total counted energy
 float wattage = 0; // what is our present measured wattage
 unsigned long lastWattCalc = 0; // when's the last time we calculated wattage
 unsigned long backupTimer = 0;
 float lastWattCalcWattHours = 0.0; // what our energy count was last time we calculated wattage
-#define WATTCALCTIME    2000 // how many milliseconds between recalculating wattage
+#define WATTCALCTIME    1000 // how many milliseconds between recalculating wattage
 
 #include <EEPROM.h>
 #define WATTHOURS_EEPROM_ADDRESS 20 // long-term memory in EEPROM
 #define BACKUP_INTERVAL 30000 // how often to write our energy to the EEPROM
-#define WATTHOUR_RESET_PIN      7
+#define WATTHOUR_RESET_PIN      6
+#define POWER_STRIP_PIN         7
+#define POWER_STRIP_PIXELS      40 // number of pixels in whatwatt power column
+#define WATTHOUR_DISPLAY_PIN    4
+#define WATTHOUR_DISPLAY_PIXELS (8*28) // actually 27 wide but leftmost doesn't exist
+// bottom right is first pixel, goes up 8, left 1, down 8, left 1...
+// https://www.aliexpress.com/item/8-32-Pixel/32225275406.html
+#define ENERGYPULSE_PIN         8 // pin 8 = PB0 aka PCINT0_vect
+#include <Adafruit_NeoPixel.h>
+#include "font1.h"
+uint32_t fontColor = Adafruit_NeoPixel::Color(25,25,25);//255,255,100);
+uint32_t backgroundColor = Adafruit_NeoPixel::Color(0,0,0);//5);
+Adafruit_NeoPixel wattHourDisplay = Adafruit_NeoPixel(WATTHOUR_DISPLAY_PIXELS, WATTHOUR_DISPLAY_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel powerStrip = Adafruit_NeoPixel(POWER_STRIP_PIXELS, POWER_STRIP_PIN, NEO_GRB + NEO_KHZ800);
+
 
 ISR(PCINT0_vect) { // fire an interrupt when PB0 changes state
   if (PINB & _BV(PB0)) wattHours += ENERGYPULSE; // this pulse means ENERGYPULSE energy was used
@@ -43,12 +50,17 @@ void setup() {
   Serial.begin(BAUDRATE);
   Serial.println(VERSIONSTR);
   digitalWrite( WATTHOUR_RESET_PIN, HIGH );  // we want to read with pullup enabled
-  load_watthours();
+  load_watthours(); // read wattHours from EEPROM
   PCICR |= _BV(PCIE0); //  Pin Change Interrupt enable on PCINT0
   PCMSK0 |= _BV(PCINT0); // enable PCINT0 for PB0
+  wattHourDisplay.begin();
+  wattHourDisplay.show();
+  powerStrip.begin(); // Initialize all pixels to 'off'
+  powerStrip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
+  updateDisplay();
   Serial.print(wattHours);
   Serial.print("\t");
   wattage = (wattHours - lastWattCalcWattHours) * 3600;
@@ -56,6 +68,43 @@ void loop() {
   lastWattCalcWattHours = wattHours;
   delay(1000);
   storeEnergy();
+}
+
+void updateDisplay() {
+  char buf[]="    "; // stores the number we're going to display
+  //char* text;
+  sprintf(buf,"%4d",1234);//(int)(wattHours * 10));
+  Serial.print("buf:");
+  Serial.println(buf);
+  writeWattHourDisplay(buf);
+}
+
+void writeWattHourDisplay(char* text) {
+#define DISPLAY_CHARS   4 // number of characters in display
+#define FONT_W 7 // width of font
+#define FONT_H 8 // height of font
+  for (int textIndex=0; textIndex<DISPLAY_CHARS; textIndex++) {
+    char buffer[FONT_H][FONT_W]; // array of horizontal lines, top to bottom, left to right
+    for(int fontIndex=0; fontIndex<sizeof(charList); fontIndex++){ // charList is in font1.h
+      if(charList[fontIndex] == text[textIndex]){ // if fontIndex is the index of the desired letter
+        int pos = fontIndex*FONT_H; // index into CHL where the character starts
+        for(int row=0;row<FONT_H;row++){ // for each horizontal row of pixels
+          memcpy_P(buffer[row], (PGM_P)pgm_read_word(&(CHL[pos+row])), FONT_W); // copy to buffer from flash
+        }
+      }
+    }
+    for (int fontXIndex=0; fontXIndex<FONT_W; fontXIndex++) {
+      for (int fontYIndex=0; fontYIndex<FONT_H; fontYIndex++) {
+        uint32_t pixelColor = buffer[fontYIndex][FONT_W-1-fontXIndex]=='0' ? fontColor : backgroundColor; // here is where the magic happens
+        if ((FONT_W*(DISPLAY_CHARS-1-textIndex) + fontXIndex) & 1) { // odd columns are top-to-bottom
+          wattHourDisplay.setPixelColor((FONT_H*FONT_W)*(DISPLAY_CHARS-1-textIndex) + fontXIndex*FONT_H +           fontYIndex ,pixelColor);
+        } else { // even columns are bottom-to-top
+          wattHourDisplay.setPixelColor((FONT_H*FONT_W)*(DISPLAY_CHARS-1-textIndex) + fontXIndex*FONT_H + (FONT_H-1-fontYIndex),pixelColor);
+        }
+      }
+    }
+  }
+  wattHourDisplay.show(); // send the update out to the LEDs
 }
 
 void storeEnergy() {
