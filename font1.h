f06c6d272b8e108362caef4d9563e35fcb56cd07
jake waters <jerkey@g-cipher.net>
2017-05-16 17:27:25 -0700

    first try with a 7-pixel wide number font

---
+++ font1.h
@@ -0,0 +1,115 @@
+//Start the letter definition and put it in the FLASH memory
+//Spc 20
+const char CH200[] PROGMEM = "       ";
+const char CH201[] PROGMEM = "       ";
+const char CH202[] PROGMEM = "       ";
+const char CH203[] PROGMEM = "       ";
+const char CH204[] PROGMEM = "       ";
+const char CH205[] PROGMEM = "       ";
+const char CH206[] PROGMEM = "       ";
+const char CH207[] PROGMEM = "       ";
+//0 30
+const char CH300[] PROGMEM = "   00  ";
+const char CH301[] PROGMEM = "  0  0 ";
+const char CH302[] PROGMEM = " 0    0";
+const char CH303[] PROGMEM = " 0    0";
+const char CH304[] PROGMEM = " 0    0";
+const char CH305[] PROGMEM = "  0  0 ";
+const char CH306[] PROGMEM = "   00  ";
+const char CH307[] PROGMEM = "       ";
+//1 31
+const char CH310[] PROGMEM = "    0  ";
+const char CH311[] PROGMEM = "   00  ";
+const char CH312[] PROGMEM = "  0 0  ";
+const char CH313[] PROGMEM = "    0  ";
+const char CH314[] PROGMEM = "    0  ";
+const char CH315[] PROGMEM = "    0  ";
+const char CH316[] PROGMEM = "  00000";
+const char CH317[] PROGMEM = "       ";
+//2 32
+const char CH320[] PROGMEM = "   00  ";
+const char CH321[] PROGMEM = "  0  0 ";
+const char CH322[] PROGMEM = "     0 ";
+const char CH323[] PROGMEM = "    0  ";
+const char CH324[] PROGMEM = "   0   ";
+const char CH325[] PROGMEM = "  0    ";
+const char CH326[] PROGMEM = " 000000";
+const char CH327[] PROGMEM = "       ";
+//3 33
+const char CH330[] PROGMEM = "  0000 ";
+const char CH331[] PROGMEM = " 0    0";
+const char CH332[] PROGMEM = "      0";
+const char CH333[] PROGMEM = "   000 ";
+const char CH334[] PROGMEM = "      0";
+const char CH335[] PROGMEM = " 0    0";
+const char CH336[] PROGMEM = "  0000 ";
+const char CH337[] PROGMEM = "       ";
+//4 34
+const char CH340[] PROGMEM = " 0   0 ";
+const char CH341[] PROGMEM = " 0   0 ";
+const char CH342[] PROGMEM = " 0   0 ";
+const char CH343[] PROGMEM = " 000000";
+const char CH344[] PROGMEM = "     0 ";
+const char CH345[] PROGMEM = "     0 ";
+const char CH346[] PROGMEM = "    000";
+const char CH347[] PROGMEM = "       ";
+//5 35
+const char CH350[] PROGMEM = " 000000";
+const char CH351[] PROGMEM = " 0     ";
+const char CH352[] PROGMEM = " 0 000 ";
+const char CH353[] PROGMEM = " 00   0";
+const char CH354[] PROGMEM = "      0";
+const char CH355[] PROGMEM = " 0    0";
+const char CH356[] PROGMEM = "  0000 ";
+const char CH357[] PROGMEM = "       ";
+//6 36
+const char CH360[] PROGMEM = "   000 ";
+const char CH361[] PROGMEM = "  0   0";
+const char CH362[] PROGMEM = " 0     ";
+const char CH363[] PROGMEM = " 0 000 ";
+const char CH364[] PROGMEM = " 00   0";
+const char CH365[] PROGMEM = " 0    0";
+const char CH366[] PROGMEM = "  0000 ";
+const char CH367[] PROGMEM = "       ";
+//7 37
+const char CH370[] PROGMEM = " 000000";
+const char CH371[] PROGMEM = " 0    0";
+const char CH372[] PROGMEM = "     0 ";
+const char CH373[] PROGMEM = "    0  ";
+const char CH374[] PROGMEM = "   0   ";
+const char CH375[] PROGMEM = "   0   ";
+const char CH376[] PROGMEM = "   0   ";
+const char CH377[] PROGMEM = "       ";
+//8 38
+const char CH380[] PROGMEM = "  0000 ";
+const char CH381[] PROGMEM = " 0    0";
+const char CH382[] PROGMEM = " 0    0";
+const char CH383[] PROGMEM = "  0000 ";
+const char CH384[] PROGMEM = " 0    0";
+const char CH385[] PROGMEM = " 0    0";
+const char CH386[] PROGMEM = "  0000 ";
+const char CH387[] PROGMEM = "       ";
+//9 39
+const char CH390[] PROGMEM = "  0000 ";
+const char CH391[] PROGMEM = " 0    0";
+const char CH392[] PROGMEM = " 0    0";
+const char CH393[] PROGMEM = "  00000";
+const char CH394[] PROGMEM = "      0";
+const char CH395[] PROGMEM = " 0   0 ";
+const char CH396[] PROGMEM = "  000  ";
+const char CH397[] PROGMEM = "       ";
+
+const char charList[ ] = {' ','0','1','2','3','4','5','6','7','8','9'};
+
+PGM_P const CHL[] PROGMEM= {
+CH200,CH201,CH202,CH203,CH204,CH205,CH206,CH207,
+CH300,CH301,CH302,CH303,CH304,CH305,CH306,CH307,
+CH310,CH311,CH312,CH313,CH314,CH315,CH316,CH317,
+CH320,CH321,CH322,CH323,CH324,CH325,CH326,CH327,
+CH330,CH331,CH332,CH333,CH334,CH335,CH336,CH337,
+CH340,CH341,CH342,CH343,CH344,CH345,CH346,CH347,
+CH350,CH351,CH352,CH353,CH354,CH355,CH356,CH357,
+CH360,CH361,CH362,CH363,CH364,CH365,CH366,CH367,
+CH370,CH371,CH372,CH373,CH374,CH375,CH376,CH377,
+CH380,CH381,CH382,CH383,CH384,CH385,CH386,CH387,
+CH390,CH391,CH392,CH393,CH394,CH395,CH396,CH397 };
